15e8ec53e23c58b28f2944a4a7429856f400e1e1
Harald Hoff <harald@hoffelec.com>
2017-07-06 16:27:36 +0200

    ...

--- baby-steps.py
+++ baby-steps.py
@@ -6,3 +6,20 @@ y = T.dscalar('y')
 z = x + y
 f = function([x, y], z)
 
+print(f)
+print(f(2, 3))
+print(numpy.allclose(f(16.3, 12.1), 28.4))
+
+
+
+
+x = T.dmatrix('x')
+y = T.dmatrix('y')
+z = x + y
+f = function([x, y], z)
+
+print(f([[1, 2], [3, 4]], [[10, 20], [30, 40]]))
+
+
+
+
