2d75b22e0f37cd60db7b250b1ae3bb6ef8c9eac2
Harald Hoff <harald@hoffelec.com>
2017-07-06 16:31:09 +0200

    %1

--- baby-steps.py
+++ baby-steps.py
@@ -23,3 +23,11 @@ print(f([[1, 2], [3, 4]], [[10, 20], [30, 40]]))
 
 
 
+import theano
+a = theano.tensor.vector()      # declare variable
+out = a + a ** 10               # build symbolic expression
+f = theano.function([a], out)   # compile function
+print(f([0, 1, 2]))
+
+
+
